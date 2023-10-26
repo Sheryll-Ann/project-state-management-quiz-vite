bb91872430e49be246ea3fbd09ce847affee2804
sheryll.poo-cheong <shey_pc@gmail.com>
2023-10-26 10:21:19 +0200

    added restart btn +connecting summary component

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,13 +1,28 @@
 import useQuizStore from "../stores/useQuizStore";
+import { Loader } from 'react-js-loader'
 
 export const Summary = () => {
   const quizOver = useQuizStore((state) => state.quizOver);
   const restart = useQuizStore((state) => state.restart);
+
+  const answers = useQuizStore((state) => state.answers);
+  // const numberOfCorrectAnswers = useQuizStore((state) => state.numberOfCorrectAnswers);
+  const numberOfCorrectAnswer = answers.filter((a) => (a.isCorrect === true));
+
+
   return (
     <div className="summary-part">
       {/* create the summary part, show the how many questions that user has answered correct
         <p>You have answered 2 correct questions</p> 
         */}
+
+      {/* <div className="item">
+                <Loader type="spinner-circle" bgColor={black} color={#f26a1b} title={"spinner-circle"} size={100} />
+            </div> */}
+      <p>Number of correct answers: {numberOfCorrectAnswer.length}</p>
+      {/* <p>{numberOfCorrectAnswers}</p> */}
+
+
       {/* show the picture when the user answered how many correct questions */}
       {/* restart button */}
       {quizOver && (
@@ -18,3 +33,4 @@ export const Summary = () => {
     </div>
   );
 };
+
